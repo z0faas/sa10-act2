26991a637a6aa063e9d033d17454f48581557e4a
Jonathan Maronde <j2mike4@gmail.com>
2024-02-28 07:47:21 -0600

    Created sulfuras_tick and backstage_tick for when item is a Sulfuras or when it's a backstage pass

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -23,12 +23,29 @@ class GildedRose
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
 
+  def sulfuras_tick
+    # nothing changes when the item is a Sulfuras
+  end
+
+  def backstage_tick
+    @days_remaining -= 1
+    return              if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+  end
+
   def tick
     case name
     when 'Normal Item'
       return normal_tick
     when 'Aged Brie'
       return brie_tick
+    when 'Sulfuras, Hand of Ragnaros'
+      return sulfuras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
     end
 
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
