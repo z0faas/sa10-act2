2c1d41a8b96aac8463657764d654780a34da81fb
Jonathan Maronde <j2mike4@gmail.com>
2024-02-28 10:08:39 -0600

    Changed GildedRose to a module, changed its initialize and klass_for to self.new and self.klass_for. Removed the tick, quality, and days remaining methods since the module calls new

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,9 @@
-class GildedRose
-  attr_reader :name :item
-
-  def initialize(name, quality, days_remaining)
+module GildedRose
+  def self.new(name, quality, days_remaining)
     @item = klass_for(name).new(quality, days_remaining)
   end
 
-  def klass_for(name)
+  def self.klass_for(name)
     case name
     when 'Normal Item'
       Normal
@@ -16,18 +14,6 @@ class GildedRose
     when 'Backstage passes to a TAFKAL80ETC concert'
       Backstage
     end
-  end
-
-  def tick
-    item.tick
-  end
-
-  def quality
-    item.quality
-  end
-
-  def days_remaining
-    item.days_remaining
   end
 
   class Normal
