dad5c472cdc36587855f1d7946c83ba20e2f3ec9
Jonathan Maronde <j2mike4@gmail.com>
2024-02-28 09:07:45 -0600

    Fixed an error where I forgot to return backstage_tick in the tick method, and I removed the original spaghetti code since it's been converted to methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -46,50 +46,7 @@ class GildedRose
     when 'Sulfuras, Hand of Ragnaros'
       return sulfuras_tick
     when 'Backstage passes to a TAFKAL80ETC concert'
-    end
-
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
+      return backstage_tick
     end
   end
 end
